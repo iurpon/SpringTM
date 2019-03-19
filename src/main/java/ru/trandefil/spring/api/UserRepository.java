bc8dcc53cacc89e5e1976eabf3bc55e82ec2beb8
trandefil <trandafir.iura@gmail.com>
2019-03-19 18:47:49 +0300

    starting data-jpa

--- src/main/java/ru/trandefil/spring/api/UserRepository.java
+++ src/main/java/ru/trandefil/spring/api/UserRepository.java
@@ -1,13 +1,38 @@
 package ru.trandefil.spring.api;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.trandefil.spring.model.User;
 
-import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
 
-public interface UserRepository {
 
-    User getLoggedUser(String userName, String password, EntityManager entityManager);
+public interface UserRepository extends JpaRepository<User, String> {
+
+    @Override
+    <S extends User> S save(S s);
+
+    @Override
+    Optional<User> findById(String s);
+
+    @Override
+    void deleteById(String s);
+
+    @Override
+    void delete(User user);
+
+    @Query("select u from User u where u.name = :name and u.password = :password")
+    User getLoggedUser(@Param("name") String name, @Param("password") String password);
+
+    @Query("select u from User u where u.name = :name")
+    User getByName(@Param("name") String name);
+
+    @Query("select u from User u")
+    List<User> getAll();
+
+    /*    User getLoggedUser(String userName, String password, EntityManager entityManager);
 
     User save(User user,EntityManager entityManager);
 
@@ -15,6 +40,6 @@ public interface UserRepository {
 
     void deleteById(String id,EntityManager entityManager);
 
-    User getById(String id,EntityManager entityManager);
+    User getById(String id,EntityManager entityManager);*/
 
 }
--- src/main/java/ru/trandefil/spring/service/UserServiceImpl.java
+++ src/main/java/ru/trandefil/spring/service/UserServiceImpl.java
@@ -8,45 +8,48 @@ import ru.trandefil.spring.api.UserRepository;
 import ru.trandefil.spring.api.UserService;
 import ru.trandefil.spring.model.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 @Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+
     @Autowired
     private UserRepository userRepository;
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
     @Override
     public User getLoggedUser(@NonNull final String userName, @NonNull final String password) {
-        return userRepository.getLoggedUser(userName, password,entityManager);
+        logger.info("==================================== user service impl getLoggedUser");
+        return userRepository.getLoggedUser(userName, password);
     }
 
     @Override
     @Transactional
     public User save(@NonNull final User user) {
-        return userRepository.save(user,entityManager);
+        logger.info("==================================== user service impl save");
+        return userRepository.save(user);
     }
 
     @Override
     public List<User> getAll() {
-        return userRepository.getAll(entityManager);
+        logger.info("==================================== user service impl getAll");
+        return userRepository.getAll();
     }
 
     @Override
     @Transactional
     public void deleteById(@NonNull final String id) {
-        userRepository.deleteById(id,entityManager);
+        logger.info("==================================== user service impl deleteById");
+        userRepository.deleteById(id);
     }
 
     @Override
     public User getById(@NonNull final String id) {
-        return userRepository.getById(id,entityManager);
+        logger.info("==================================== user service impl getById");
+        return userRepository.findById(id).orElse(null);
     }
 
 }
